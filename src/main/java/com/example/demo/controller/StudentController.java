fee99482ade9c6d46a2f11dc031e62d014470234
bala-amypo <bala@amypo.in>
2025-12-17 09:21:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.StudentEntity;
+import org.springframework.stereotype.Restontroller;
+
+@Restcontroller
 public class StudentController {
     
 }
