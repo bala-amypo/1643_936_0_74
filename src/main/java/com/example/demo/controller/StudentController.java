db778223db6f7550404e763c1c23bffa8703b105
bala-amypo <bala@amypo.in>
2025-12-17 09:26:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.StudentEntity;
-import org.springframework.stereotype.Restontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.RestController;
 
 @Restcontroller
 public class StudentController {
-    
+    @Autowired StudentService 
 }
--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -2,9 +2,9 @@ package com.example.demo.service.Impl;
 
 import com.example.demo.entity.StudentEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.stereotype.Autowired;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class StudentServiceImpl implements StudentService{
