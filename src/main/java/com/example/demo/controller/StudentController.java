fa256396f506c3cf5076aaabb8df65061337c6e4
bala-amypo <bala@amypo.in>
2025-12-18 05:03:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.example.demo.service.StudentService;
 import com.example.demo.entity.StudentEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -20,7 +21,7 @@ public class StudentController {
     public List<StudentEntity> getAll(){
         return service.getAllData();
     }
-    @GetMapping("/del")
+    @GetMapping("/del/{id}")
     public String DeleteData(int id){
         return service.DeleteData(id);
     }
