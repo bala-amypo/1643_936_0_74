6b5b78b35408376b60fcd78aa7bdf89fc4a88ef4
bala-amypo <bala@amypo.in>
2025-12-19 09:24:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -37,48 +37,8 @@ public class TimeStampEntity{
     private String username;
     @Email
     private String email;
-
-    @NotNull(message="Password is required")
-    package com.example.demo.entity;
-
-import java.util.Date;
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Id;
-import java.time.LocalDateTime;
-import jakarta.persistence.GeneratedValue;
-import jakarta.validation.constraints.Max;
-import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Positive;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Data;
-import java.time.LocalDateTime;
-import jakarta.persistence.Table;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "TimeStamp")
-
-public class TimeStampEntity{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     @NotNull
-    @Size(min=5, max=10, message="Must be atleast 5 to 10 characters")
-    private String username;
-    @Email(message="Email not valid")
-    private String email;
-    @NotNull(message="Password is required")
+
     private LocalDateTime createdat;
     private LocalDateTime editedat;
     @PrePersist
@@ -92,5 +52,4 @@ public class TimeStampEntity{
         LocalDateTime now = LocalDateTime().now();
         this.editedat = now;
     }
-}
 }
