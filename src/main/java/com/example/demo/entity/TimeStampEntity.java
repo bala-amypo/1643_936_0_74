a26474382600623d5eb4cb11efc4e7dbd83164b1
bala-amypo <bala@amypo.in>
2025-12-19 09:08:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -1,54 +1,47 @@
 package com.example.demo.entity;
 
-import java.util.Date;
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Id;
-import java.time.LocalDateTime;
-import jakarta.persistence.GeneratedValue;
-import jakarta.validation.constraints.Max;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Positive;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Data;
-import java.time.LocalDateTime;
-import jakarta.persistence.Table;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "time_stamp")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "TimeStamp")
+public class TimeStampEntity {
 
-public class TimeStampEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
-    @Size(min=5, max=10, message="Must be atleast 5 to 10 characters")
+    @Size(min = 5, max = 10, message = "Must be between 5 and 10 characters")
     private String username;
-    @Email(message="Email not valid")
+
+    @Email(message = "Email not valid")
     private String email;
-    @NotNull(message="Password is required")
-    private LocalDateTime createdat;
-    private LocalDateTime editedat;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime editedAt;
+
     @PrePersist
-    public void Oncreate(){
-        LocalDateTime now = LocalDateTime().now();
-        this.createdat = now;
-        this.editedat = now;
+    public void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.editedAt = now;
     }
+
     @PreUpdate
-    public void Onupdate(){
-        LocalDateTime now = LocalDateTime().now();
-        this.editedat = now;
+    public void onUpdate() {
+        this.editedAt = LocalDateTime.now();
     }
 }
