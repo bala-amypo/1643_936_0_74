0bb1e8db0fc6581afe39374f18d2068152797881
bala-amypo <bala@amypo.in>
2025-12-19 03:59:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/ValidationException.java
+++ src/main/java/com/example/demo/exception/ValidationException.java
@@ -3,16 +3,16 @@ package com.example.demo.exception;
 public class ValidationException extends RunTimeException{
     public ValidationException(String message){
         super(message);
-        public ValidationEntity FindId(@PathVariable Long id){
+        public Validation FindId(@PathVariable Long id){
             return service.FindId(id);
         }
-        public ValidationEntity FindId(@PathVariable Integer id){
+        public Validation FindId(@PathVariable Integer id){
             return service.FindId(id);
         }
         public Validation FindId(Long id){
             return student.findById(id);
         }
-        public ValidationEntity FindId(Integer id){
+        public Validation FindId(Integer id){
             return student.findById(id);
         }
     }
