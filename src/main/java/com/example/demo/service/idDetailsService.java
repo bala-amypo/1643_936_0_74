a78fbc3713aff151dbff304ce696f06c437a6878
bala-amypo <bala@amypo.in>
2025-12-19 10:12:28 +0000

    User commit

--- src/main/java/com/example/demo/service/idDetailsService.java
+++ src/main/java/com/example/demo/service/idDetailsService.java
@@ -1,31 +1,31 @@
 package com.example.demo.service.Impl;
 
-import com.example.demo.entity.StudentDetailsEntity;
+import com.example.demo.entity.idStudentEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import com.example.demo.repository.StudetDetailsRepository;
-import com.example.demo.service.StudetDetailsService;
+import com.example.demo.repository.idDetailsRepository;
+import com.example.demo.service.idDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
-public class StudetDetailsServiceImpl{
-    @Autowired StudetDetailsRepository student;
+public class idtDetailsServiceImpl{
+    @Autowired idDetailsRepository student;
     
-    public StudetDetailsEntity Postdata(StudetDetailsEntity entity){
+    public idDetailsEntity Postdata(idDetailsEntity entity){
         return student.save(entity);
     }
-    public List<StudetDetailsEntity>getAllData(){
+    public List<idDetailsEntity>getAllData(){
         return student.findAll();
     }
     public String DeleteData(int id){
         student.deleteById(id);
         return "Deleted Succesfully";
     }
-    public StudetDetailsEntity FindId(int id){
+    public idDetailsEntity FindId(int id){
         return student.findById(id).orElse(null);
     }
-    public StudetDetailsEntity PutData(int id, StudetDetailsEntity entity){
+    public idDetailsEntity PutData(int id, idDetailsEntity entity){
         if(student.existsById(id)){
             entity.setId(id);
             return student.save(entity);
