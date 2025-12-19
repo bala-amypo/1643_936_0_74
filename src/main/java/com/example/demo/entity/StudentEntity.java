3609c94d97d2ca6d6c0352a7973af84c4a069148
bala-amypo <bala@amypo.in>
2025-12-19 06:26:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,22 +1,29 @@
 package com.example.demo.entity;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class StudentEntity{
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String Username;
-    private String Email;
-    private String Password;
-    private Date Created;
 
-    
-
+    private String username;
+    private String email;
+    private String password;
+    private LocalDateTime created;
 }
-
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -10,7 +10,16 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class ValidationEntity{
     @Id
@@ -29,53 +38,4 @@ public class ValidationEntity{
     @Positive
     private int age;
 
-    //Getter Method
-    public Long getId(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public int getAge(){
-        return age;
-    } 
-
-    //Setter Method
-    public void setId(Long id){
-        this.id = id;
-    }
-    public void setUsername(String name){
-        this.username = name;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public void setPassword(String pass){
-        this.password = pass;
-    }
-    public void setAge(int age){
-        this.age = age;
-    }
-
-    public ValidationEntity(Long id, 
-            @NotNull @Size(min=5, max=10, message="Must be atleast 5 to 10 characters") String username,
-            @Email(message="Email not valid") String email,
-            @NotNull(message="Password is required") @Size(max=6, message="Password should be at max 6 letters") String password,
-            @NotNull(message="Age is required") @Max(30) @Positive int age){
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.age = age;
-    }
-
-    public ValidationEntity(){
-
-    }
 }
