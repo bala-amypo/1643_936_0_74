5243b6b1788dd8ee6fa7171a099a7dfa337178d3
bala-amypo <bala@amypo.in>
2025-12-19 07:55:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/TimeStampRepository.java
+++ src/main/java/com/example/demo/repository/TimeStampRepository.java
@@ -2,9 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ValidationEntity;
+import com.example.demo.entity.TimeStampEntity;
 
 @Repository
-public interface ValidationRepository extends JpaRepository<ValidationEntity, Long>{
+public interface TimeStampRepository extends JpaRepository<TimeStampEntity, Long>{
 
 }
--- src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
@@ -1,10 +1,10 @@
 package com.example.demo.service.Impl;
 
-import com.example.demo.entity.ValidationEntity;
+import com.example.demo.entity.TimeStampEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import com.example.demo.repository.ValidationRepository;
-import com.example.demo.service.ValidationService;
+import com.example.demo.repository.TimeStampRepository;
+import com.example.demo.service.TimeStampService;
 import com.example.demo.exception.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 
