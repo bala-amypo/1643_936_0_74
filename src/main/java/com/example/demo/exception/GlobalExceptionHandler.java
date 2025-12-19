ebed9939916334b3f9e819aa1a75cb36692b8824
bala-amypo <bala@amypo.in>
2025-12-19 05:27:08 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package com.example.demo.exception;
 import java.lang.RuntimeException;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException{
     public GlobalExceptionHandler(String message){
         super(message);
