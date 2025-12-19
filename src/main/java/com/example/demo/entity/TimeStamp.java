82e76a8f0d5563d2ae9bae413d75c1497fb3ed4c
bala-amypo <bala@amypo.in>
2025-12-19 07:42:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/TimeStamp.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.Max;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import jakarta.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "validation")
+
+public class TimeStampEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min=5, max=10, message="Must be atleast 5 to 10 characters")
+    private String username;
+    @Email(message="Email not valid")
+    private String email;
+    @NotNull(message="Password is required")
+    @Size(max=5, message="Password should be at max 6 letters")
+    private String password;
+    @NotNull(message="Age is required")
+    @Max(30)
+    @Positive
+    private int age;
+
+}
